bb43acd1c182903daab51e2724b46767a3807748
Pierre C <cilluffo.jeuxvideo@outlook.fr>
2024-12-05 15:46:25 +0100

    Initial Commit

---
+++ ZakFit_SwiftUI/ZakFit_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZakFit_SwiftUIApp.swift
+//  ZakFit_SwiftUI
+//
+//  Created by Apprenant 141 on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ZakFit_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
